8526f64a85c94b1813b810cc06b2f6d8edf93c1a
Vavas <vavas-terran@mail.ru>
2020-12-07 20:55:40 +0300

    add files

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  var btn = $('.scroll-to-top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+
+
+})(jQuery); 
