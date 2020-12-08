e484362719bdb67089d58149cc1e57dcd549db5e
Vavas <vavas-terran@mail.ru>
2020-12-09 01:52:48 +0300

    add slider

--- src/js/main.js
+++ src/js/main.js
@@ -33,9 +33,14 @@
   $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        adaptiveHeight: false,
+        arrows: true,
+        dots: true,
+
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider_right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
