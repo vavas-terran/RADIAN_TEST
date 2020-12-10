62dacf6ca1bdc139f7e6431cc3aa85775fc493b2
Vavas <vavas-terran@mail.ru>
2020-12-10 13:05:08 +0300

    finished

--- src/js/main.js
+++ src/js/main.js
@@ -79,6 +79,16 @@
 
 })(jQuery); 
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 
 
 	
