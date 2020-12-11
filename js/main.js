32fba772c2a56709126bcee01faa1cb43fa6517e
Vavas <31109839+vavas-terran@users.noreply.github.com>
2020-12-11 09:32:53 +0300

    finished test

---
+++ js/main.js
@@ -0,0 +1,95 @@
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  var btn = $('.scroll-to-top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+
+
+
+
+  $(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        adaptiveHeight: false,
+        arrows: true,
+        dots: true,
+
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider_right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+
+
+
+
+
+
+})(jQuery); 
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+	
+
